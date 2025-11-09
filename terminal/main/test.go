b0690d855099f685cdd85ba9fd6f05385cffc003
DinithBJayalath <dinithbjayalath@gmail.com>
2025-11-09 18:03:42 +0530

    customised the initial cwd theme

--- terminal/main/test.go
+++ terminal/main/test.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"github.com/fyne-io/terminal"
+
+	"terminal/ui"
 )
 
 func main() {
 	termApp := app.New()
 	termWindow := termApp.NewWindow("Zag")
-	term := terminal.New()
-
-	go func() {
-		_ = term.RunLocalShell()
-		fmt.Printf("Shell exited: %d", term.ExitCode())
-		termApp.Quit()
-	}()
-
+	term := ui.AttachTerminal(termApp)
 	termWindow.SetContent(container.NewStack(term))
 	termWindow.Resize(fyne.NewSize(900, 600))
 	termWindow.ShowAndRun()
---
+++ terminal/ui/termWidget.go
@@ -0,0 +1,30 @@
+package ui
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"fyne.io/fyne/v2"
+	"github.com/fyne-io/terminal"
+)
+
+func AttachTerminal(termApp fyne.App) fyne.CanvasObject {
+	//Setting up the initial path
+	os.Setenv("SHELL", "/bin/zsh")
+	tmp, _ := os.MkdirTemp("", "zag-zdotdir-")
+	_ = os.WriteFile(filepath.Join(tmp, ".zshrc"), []byte(`
+	setopt PROMPT_SUBST
+	PROMPT='%F{#018ADA}%2~ ❯%f '
+	RPROMPT=''
+	`), 0600)
+	os.Setenv("ZDOTDIR", tmp)
+	// Setting up the terminal widget
+	term := terminal.New()
+	go func() {
+		_ = term.RunLocalShell()
+		log.Printf("Shell exited: %d", term.ExitCode())
+		termApp.Quit()
+	}()
+	return term
+}
