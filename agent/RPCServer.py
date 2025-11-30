b6c2efe1ad66aaa52e2ba4841e0401ef5c364867
DinithBJayalath <dinithbjayalath@gmail.com>
2025-11-30 09:37:46 +0530

    Fixed import bugs for gRPC server

--- agent/RPCServer.py
+++ agent/RPCServer.py
@@ -1,5 +1,5 @@
-from nlpAgent_pb2_grpc import NLAgentServicer, add_RouteGuideServicer_to_server
-from nlpAgent_pb2 import LLMResponse
+from grpcService.nlpAgent_pb2_grpc import NLAgentServicer, add_NLAgentServicer_to_server
+from grpcService.nlpAgent_pb2 import LLMResponse
 import grpc
 from concurrent import futures
 from main import processCommand
@@ -12,7 +12,11 @@ class NLAgentService(NLAgentServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_RouteGuideServicer_to_server(NLAgentService, server)
+    add_NLAgentServicer_to_server(NLAgentService, server)
     server.add_insecure_port("[::]:50051")
     server.start()
+    print("Server stated")
     server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
